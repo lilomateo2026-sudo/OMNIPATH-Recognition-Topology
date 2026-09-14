2ec229e9eac65a5722dd72b9b2165da3f64cbec3
lilomateo2026-sudo <lilomateo2026@gmail.com>
2026-09-14 03:25:49 -0400

    O31.7: update recovery proof test

--- omnipath/recognition/tests/test_o31_5_recovery.py
+++ omnipath/recognition/tests/test_o31_5_recovery.py
@@ -6,6 +6,6 @@ def test_recovery_proof():
     x=json.loads(P.read_text()); seal=x.pop('proof_set_sha256')
     assert seal==hashlib.sha256(json.dumps(x,sort_keys=True,separators=(',',':')).encode()).hexdigest()
     p=x['proofs'][0]
-    assert x['order']=='O31.5'
+    assert x['order']=='O31.7'
     assert p['classification']=='HISTORICALLY_REACHABLE'
     assert p['expected_blob_sha']==p['observed_path_blob_sha']==p['git_hash_object_sha']
